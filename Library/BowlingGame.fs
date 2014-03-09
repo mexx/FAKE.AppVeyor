6ed6d6e65ceddfbe00a50998a4ef84fb274f719f
Max Malook <community@malook.de>
2014-03-09 22:06:45 +0100

    add compiler warning

--- Library/BowlingGame.fs
+++ Library/BowlingGame.fs
@@ -1,4 +1,13 @@
 ﻿module BowlingGame
 
+type Score =
+    | Strike
+    | Spare
+    | Try of int
+    | Miss
+
 let score tries =
+    let decide x =
+        match x with
+        | Miss -> 123
     0
