6e4f56020ed1e5ae5e1b94dfbb0c486ec7c16d06
Max Malook <community@malook.de>
2014-03-09 04:04:58 +0100

    add trace of environment variables

--- build.fsx
+++ build.fsx
@@ -3,7 +3,14 @@
 
 open Fake
 
+TraceEnvironmentVariables()
+
 RestorePackages()
+//if buildServer = BuildServer.AppVeyor then
+//    MSBuildDefaults <- { MSBuildDefaults with 
+//                            FileLoggers = Some [{ Number = 1; Filename = None; Verbosity = None; Parameters = None}];
+//                            Properties = ["logger","C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"]
+//        }
 
 Target "Clean" (fun _ ->
     !! ("**/bin/**/*.*")
